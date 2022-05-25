0f895f6eacb18ae129de954cd89499f520ecc90d
DiegoOrtiz8 <diego.carranza7591@alumno.udg.mx>
2022-05-25 00:08:56 -0500

    Excepciones agregadas

--- src/main/java/com/ibm/academia/apirest/services/TarjetaDAOImpl.java
+++ src/main/java/com/ibm/academia/apirest/services/TarjetaDAOImpl.java
@@ -2,17 +2,15 @@ package com.ibm.academia.apirest.services;
 
 import com.ibm.academia.apirest.dto.TarjetaDTO;
 import com.ibm.academia.apirest.entities.Tarjeta;
+import com.ibm.academia.apirest.exceptions.BadRequestException;
 import com.ibm.academia.apirest.exceptions.NotFoundException;
 import com.ibm.academia.apirest.mapper.TarjetaMapper;
 import com.ibm.academia.apirest.repositories.TarjetaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -33,8 +31,18 @@ public class TarjetaDAOImpl extends GenericoDAOImpl<Tarjeta, TarjetaRepository> 
     @Transactional(readOnly = true)
     public Iterable<TarjetaDTO> obtenerRecomendacionTarjetas(String pasion, Integer edad, Double sueldo) {
 
+        if(pasion.isBlank())
+            throw new BadRequestException("Pasion no puede ser vacio");
+        if(edad < 18 || edad > 75)
+            throw new BadRequestException("El rango de Edad permitido es de 18-75 años");
+        if(sueldo < 7000.0)
+            throw new BadRequestException("El sueldo minimo es de 7,000.0");
+        if(!(pasion.contains("shopping") || pasion.contains("mystyle") || pasion.contains("mybusiness")
+                || pasion.contains("travels") || pasion.contains("sports") || pasion.contains("help")))
+            throw new BadRequestException("Ingrese una pasion valida");
+
         List<TarjetaDTO> tarjetaDTOS = null;
-        List<Tarjeta> tarjetas = (List<Tarjeta>) repository.obtenerRecomendacionTarjetas(pasion, edad, sueldo);
+        List<Tarjeta> tarjetas = (List<Tarjeta>) repository.obtenerRecomendacionTarjetas(pasion.toLowerCase(), edad, sueldo);
 
         if(tarjetas.isEmpty())
             throw new NotFoundException("No fue posible recomendar tarjetas");
