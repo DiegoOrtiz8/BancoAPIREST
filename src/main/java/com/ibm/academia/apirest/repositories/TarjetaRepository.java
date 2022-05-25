7c478063f46e3cb8cda2fede9a1740264ab5f14d
DiegoOrtiz8 <diego.carranza7591@alumno.udg.mx>
2022-05-24 22:39:33 -0500

    import sql implementado

--- src/main/java/com/ibm/academia/apirest/repositories/TarjetaRepository.java
+++ src/main/java/com/ibm/academia/apirest/repositories/TarjetaRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public interface TarjetaRepository extends PagingAndSortingRepository<Tarjeta, Integer> {
 
-    @Query("select t from Tarjeta t where t.pasion = ?1 and ?2 between t.edadMinima and t.edadMaxima " +
+    @Query("select t from Tarjeta t where t.pasion = ?1 and (?2 between t.edadMinima and t.edadMaxima) " +
             "and t.sueldoMinimo <= ?3 and t.sueldoMaximo >= ?3")
     public Iterable<Tarjeta> obtenerRecomendacionTarjetas(String pasion, Integer edad, Double sueldo);
 }
